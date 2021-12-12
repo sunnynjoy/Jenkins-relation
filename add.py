39ab258f59f1c34cc9946fa84caf6d36fc7611ec
sunny <sunny.ghosh26@gmail.com>
2021-12-12 00:36:39 +0000

    initial commiit

---
+++ add.py
@@ -0,0 +1,7 @@
+class Add:
+
+    def add_2_numbers(self, number1, number2):
+        return number1 + number2
+
+    def add_3_numbers(self, number1, number2, number3):
+        return number1 + number2 + number3
---
+++ add_main.py
@@ -0,0 +1,11 @@
+from add import Add
+
+
+class AddMain:
+
+    add = Add()
+    value = add.add_2_numbers(1, 2)
+    print(" The addition of 2 numbers is ", value)
+
+    value = add.add_3_numbers(9, 2, 1)
+    print(" The addition of 3 numbers is ", value)
