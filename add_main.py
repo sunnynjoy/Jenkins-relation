053a82e0938886075ca705f1a0e898f9bff8f2da
sunny <sunny.ghosh26@gmail.com>
2021-12-12 01:44:23 +0000

    initial commiit

--- add_main.py
+++ add_main.py
@@ -7,5 +7,6 @@ class AddMain:
     value = add.add_2_numbers(1, 2)
     print(" The addition of 2 numbers is ", value)
 
+    # 3 values
     value = add.add_3_numbers(9, 2, 1)
     print(" The addition of 3 numbers is ", value)
